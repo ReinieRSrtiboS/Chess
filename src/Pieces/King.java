6ce169ae77b4ca98338fe4c94445e721935e827c
ReinieRSrtiboS <reinierstribos@gmail.com>
2017-07-15 13:46:25 +0200

    implemented King

--- src/Pieces/King.java
+++ src/Pieces/King.java
@@ -1,6 +1,11 @@
 package Pieces;
 
+import Model.Board;
 import Model.Colour;
+import Model.Location;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class King extends Piece {
 
@@ -11,8 +16,25 @@ public class King extends Piece {
         castle = true;
     }
 
-//    @Override
-//    public Boolean isLegal(int newX, int newY) {
-//        return (-1 <= newX - x || newX - x <= 1) && (-1 <= newY - y || newY - y <= 1);
-//    }
+    @Override
+    public Boolean isLegal(Location oldLocation, Location newLocation, Board board) {
+        int difX = oldLocation.getX() - newLocation.getX();
+        int difY = oldLocation.getY() - newLocation.getY();
+        return !isFriendly(newLocation) && difX >= -1 && difX <= 1 && difY >= -1 && difY <= 1;
+    }
+
+    @Override
+    public List<Location> isAttacking(Location location, Board board) {
+        List<Location> result = new ArrayList<>();
+        int x = location.getX();
+        int y = location.getY();
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (board.inBorder(x + i, y + j)) {
+                    result.add(board.board[x+i][y+j]);
+                }
+            }
+        }
+        return result;
+    }
 }
